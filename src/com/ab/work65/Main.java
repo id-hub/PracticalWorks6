5741744248c5e1e2bc64fa908318e0972f4f8971
DIvashina <Dmitriy.Ivashina@alfabank.kiev.ua>
2020-02-05 03:22:28 +0200

    Решение заданий 6-4, 6-5

--- src/com/ab/work65/Main.java
+++ src/com/ab/work65/Main.java
@@ -1,17 +1,32 @@
 package com.ab.work65;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter height of pyramid -> ");
+        int number = sc.nextInt();
+        printPyramid(number);
+    }
 
     public static void printPyramid(int height) {
-
         int row = 1;
-        int col = 1;
-        while (col <= height) {
-
-
+        while (row <= height) {
+            String result = "" + row;
+            int col = 1;
+            while (col < height) {
+                if (col < row) {
+                    result = (row - col) + result + (row - col);
+                } else {
+                    result = " " + result + " ";
+                }
+                col++;
+                if (col == height) {
+                    System.out.println(result);
+                }
+            }
+            row++;
         }
-
-}
-
+    }
 }
